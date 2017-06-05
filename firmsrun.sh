24dbfb7b5d9b80f97ec11d69c3593780cb320f3a
Christine Waigl <cwaigl@chinook00.(none)>
2017-06-04 17:49:51 -0800

    Added VIIRS-I synching, restart upon timeout

--- firmsrun.sh
+++ firmsrun.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -l
 module purge
 module load python
-module load proj
 
-cd /u1/uaf/waigl/virtualenvs/scipy2_sitepack/bin
-source activate
-
-cd /datadir/UAFSMOKE/src/smokescripts2015
+source activate py27_uafsmoke
+cd /center1/d/UAFSMOKE/src/smokescripts2015
 python syncfirms.py
+source deactivate
--- syncfirms.py
+++ syncfirms.py
@@ -4,31 +4,44 @@ from __future__ import print_function, unicode_literals
 import datetime as dt
 import os
 import ftputil
+from credentials import FIRMS
+from time import sleep
 
-SERVER = "nrt1.modaps.eosdis.nasa.gov"
-UN = "cwaigl"
-PW = "Gerund10"
+SERVER = FIRMS["server"]
+UN = FIRMS['username']
+PW = FIRMS['cred']
 RPTH0 = "FIRMS"
-RPTH1 = "Alaska"
-RPTH2 = "Canada"
-LOCALDIR = "/datadir/UAFSMOKE/dat/fires_data/FIRMS_MOD14"
+PROD = {'MODIS': 'c6',
+    'VIIRS-I': 'viirs' 
+}
+LOCPTH = ['Alaska', 'Canada']
+LOCALDIR = "/center1/d/UAFSMOKE/dat/fires_data/"
 
-def getfiles(area="Alaska"):
-
+def getfiles(prod='c6', area="Alaska"):
     localfiles = os.listdir('.')
     with ftputil.FTPHost(SERVER, UN, PW) as host:
-        path = os.path.join(RPTH0, area)
+        path = os.path.join(RPTH0, prod, area)
         host.chdir(path)
         names = host.listdir(host.curdir)
         for fn in names:
             if fn not in localfiles:
-                print("Getting {}".format(fn))
                 host.download(fn, fn)
+                print("Retrieved {}".format(fn))
 
 def main():
-    os.chdir(LOCALDIR)
-    getfiles(RPTH1)
-    getfiles(RPTH2)
+    while True:
+        try:
+            for prodname in PROD:
+                newdir = os.path.join(LOCALDIR, RPTH0+'_'+prodname)
+                os.chdir(newdir)
+                for area in LOCPTH:
+                    print("Retrieving {}, {}".format(prodname, area))
+                    getfiles(prod=PROD[prodname], area=area)
+        except ftputil.error.FTPIOError:
+            print("Retrying...")
+            sleep(5)
+        else:
+            return
 
 if __name__ == "__main__":
     main()
