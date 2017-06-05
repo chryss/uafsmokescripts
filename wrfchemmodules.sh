f6b5d57b0bf396b2732cde9dc3b021a5533905f8
Christine Waigl <cwaigl@chinook00.(none)>
2017-06-04 17:33:45 -0800

    Added script to load correct modules

---
+++ wrfchemmodules.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+module purge
+module load toolchain/pic-intel/2016b
+module load data/netCDF-Fortran/4.4.4-pic-intel-2016b
+module load vis/JasPer/1.900.1-pic-intel-2016b
+module load lib/libpng/1.6.24-pic-intel-2016b
+module load slurm
+module load python
