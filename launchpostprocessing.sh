b3989b47251c1510f2e8d37420562ef1684e5417
Chris Waigl <cwaigl@alaska.edu>
2017-06-29 19:32:36 -0800

    new script to be executed once wrf run has finished

---
+++ launchpostprocessing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPTDIR=/center1/d/UAFSMOKE/src/scripts
+WRFDIR=/center1/d/UAFSMOKE/src/WRFV3/test/em_real/
+FLAG=POSTPROCESS.ME
+LOCK=POSTPROCESS.LOCK
+
+FILE=${WRFDIR}${FLAG}
+cd ${SCRIPTDIR}
+if [ -f $FILE ] && [ ! -f $LOCK ]; then
+    touch $LOCK
+    bash ./postprocess.sh
+fi
+cd ${SCRIPTDIR}
+rm $LOCK
+rm $FILE
