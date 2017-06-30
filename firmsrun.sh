66fdd1f64ba284eca37ef454de0515fb205c9521
Chris Waigl <cwaigl@alaska.edu>
2017-06-29 19:26:45 -0800

    this should be run as a login shell

--- firmsrun.sh
+++ firmsrun.sh
@@ -3,6 +3,10 @@ module purge
 module load python
 
 source activate py27_uafsmoke
-cd /center1/d/UAFSMOKE/src/smokescripts2015
-python syncfirms.py
+cd /center1/d/UAFSMOKE/src/scripts
+python syncfirms.py --archive --days 5
+cd /center1/d/UAFSMOKE/dat/fires_data/FIRMS_MODIS
+python preparefiresource_coarse.py --date `date -u +%Y%m%d`
+cd /center1/d/UAFSMOKE/dat/fires_data/FIRMS_VIIRS-I
+python preparefiresource_coarse.py --date `date -u +%Y%m%d`
 source deactivate
