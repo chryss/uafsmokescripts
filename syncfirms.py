ad5a5b17ab7f50c657685897b2e3264e35b3a6b6
Chris Waigl <cwaigl@alaska.edu>
2017-06-29 19:30:20 -0800

    moved to scripts directory, added command line arguments for flexibility, archiving

--- syncfirms.py
+++ syncfirms.py
@@ -3,7 +3,10 @@
 from __future__ import print_function, unicode_literals
 import datetime as dt
 import os
+import glob
+import shutil
 import ftputil
+import argparse
 from credentials import FIRMS
 from time import sleep
 
@@ -16,19 +19,61 @@ PROD = {'MODIS': 'c6',
 }
 LOCPTH = ['Alaska', 'Canada']
 LOCALDIR = "/center1/d/UAFSMOKE/dat/fires_data/"
+DEFAULTDAYS = 3
+ARCHIVE = 'archive'
 
-def getfiles(prod='c6', area="Alaska"):
+def parse_arguments():
+    """Parse arguments"""
+    parser = argparse.ArgumentParser(description='Sync near real-time (NRT) fire products from FIRMS')
+    parser.add_argument('--days',  
+        help='how many most recent days to retrieve',
+        default=DEFAULTDAYS,
+        type=int)
+    parser.add_argument('--archive',
+            help='archive all existing',
+            action='store_true')
+    parser.add_argument('--nc',
+            help= 'do not clobber',
+            action='store_true')
+    return parser.parse_args()
+
+def getfiles(prod='c6', area="Alaska", days=DEFAULTDAYS, noclobber=False):
     localfiles = os.listdir('.')
+    selectfiles = []
+    today = dt.datetime.utcnow()
+    daypatt = []
+    for delta in range(days):
+        newjulian = (today - dt.timedelta(days=delta)).strftime('%Y%j.txt')
+        daypatt.append(newjulian)
     with ftputil.FTPHost(SERVER, UN, PW) as host:
         path = os.path.join(RPTH0, prod, area)
         host.chdir(path)
         names = host.listdir(host.curdir)
-        for fn in names:
-            if fn not in localfiles:
-                host.download(fn, fn)
-                print("Retrieved {}".format(fn))
+        for patt in daypatt:
+            for name in names:
+                if name.endswith(patt):
+                    if noclobber:
+                        if name in localfiles:
+                            continue
+                    selectfiles.append(name)
+        for fn in selectfiles:
+            host.download(fn, fn)
+            print("Retrieved {}".format(fn))
 
 def main():
+    args = parse_arguments()
+    if args.archive:
+        for prodname in PROD:
+            workingdir = os.path.join(LOCALDIR, RPTH0+'_'+prodname)
+            os.chdir(workingdir)
+            files = glob.glob('*.txt')
+            print(files)
+            for fn in files:
+                if os.path.exists(os.path.join(workingdir, ARCHIVE, fn)):
+                    os.remove(os.path.join(workingdir, ARCHIVE, fn))
+                shutil.move(fn, os.path.join(workingdir, ARCHIVE))
+                print("Archived {}.".format(fn))
+    
     while True:
         try:
             for prodname in PROD:
@@ -36,7 +81,7 @@ def main():
                 os.chdir(newdir)
                 for area in LOCPTH:
                     print("Retrieving {}, {}".format(prodname, area))
-                    getfiles(prod=PROD[prodname], area=area)
+                    getfiles(prod=PROD[prodname], area=area, days=args.days, noclobber=args.nc)
         except ftputil.error.FTPIOError:
             print("Retrying...")
             sleep(5)
