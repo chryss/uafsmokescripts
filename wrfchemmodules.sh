62aa4bbc0e3560889a7d5ddf07d0847e430757b9
Chris Waigl <cwaigl@alaska.edu>
2017-06-29 19:26:13 -0800

    make sourceable

--- wrfchemmodules.sh
+++ wrfchemmodules.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash -l
 
 module purge
 module load toolchain/pic-intel/2016b
 module load data/netCDF-Fortran/4.4.4-pic-intel-2016b
+module load data/netCDF-C++4/4.3.0-pic-intel-2016b
 module load vis/JasPer/1.900.1-pic-intel-2016b
 module load lib/libpng/1.6.24-pic-intel-2016b
 module load slurm
-module load python
+module list
