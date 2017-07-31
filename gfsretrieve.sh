a52b57e4d027414c281526ab2b4c4aa8e5e68f98
Chris Waigl <cwaigl@alaska.edu>
2017-07-31 11:28:24 -0800

    Make script configurable by date/time slice/directory

--- gfsretrieve.sh
+++ gfsretrieve.sh
@@ -1,8 +1,22 @@
 #!/bin/bash -l
 # Retrieve the day's GFS forecast from Ji Zhang's download
-cd /center1/d/UAFSMOKE/dat/gfs
-rm -f gfs*
+TARGETDIR=/center1/d/UAFSMOKE/src/WPS/gfs
 #scp waigl@pacman2.arsc.edu:/import/c/w/jizhang/rt_wrf/gribfiles-gfs/`date -u +"%Y%j"`00/gfs* .
+
+while [[ $# -ge 1 ]]
+do
+    key="$1"
+    case $key in
+        -d|--directory)
+            TARGETDIR=$2
+            shift
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
 
 if [ $# -gt 1 ]; then
     WHEN=$2
@@ -15,4 +29,6 @@ else
     DATESTAMP=`date -u +"%Y%m%d"`
 fi
 
-wget "ftp://ftpprd.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.${DATESTAMP}${WHEN}/gfs.t00z.pgrb2.0p50.f0??"
+cd $TARGETDIR
+rm -f gfs*
+wget "ftp://ftpprd.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.${DATESTAMP}${WHEN}/gfs.t${WHEN}z.pgrb2.0p50.f0??"
